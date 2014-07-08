42e2ac91e8b64f533a19f0959b8431485e360a82
EmiCB <emibrown02@gmail.com>
2014-07-08 16:56:11 -0500

    Even More Files!
    Through episode 4

---
+++ src/Main/Java/com/emicb/thehuntermod/TheHunterMod.java
@@ -0,0 +1,31 @@
+package com.emicb.thehuntermod;
+
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+import cpw.mods.fml.common.event.FMLPostInitializationEvent;
+
+@Mod(modid="TheHunterMod", name="The Hunter Mod", version="1.7.2-1.0")
+public class TheHunterMod
+{
+	@Mod.Instance("TheHunterMod")
+	public static TheHunterMod instance;
+	
+	@Mod.EventHandler
+	public void preInit(FMLPreInitializationEvent event)
+	{
+		
+	}
+	
+	@Mod.EventHandler
+	public void init(FMLInitializationEvent event)
+	{
+		
+	}
+	@Mod.EventHandler
+	public void postInit(FMLPostInitializationEvent event)
+	{
+		
+	}
+	
+}
